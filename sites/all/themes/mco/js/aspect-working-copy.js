776e5a39e25283d5c4db749f2d7c78ea6f32bafb
Martin Owen <martin.j.owen@btinternet.com>
2013-08-16 08:17:24 +0100

    Updated file aspect.js
    Removed google analytics and removed block with JS.
    Updated file aspect.js and updated mco.info
    
    Signed-off-by: Martin Owen <martin.j.owen@btinternet.com>

--- sites/all/themes/mco/js/aspect-working-copy.js
+++ sites/all/themes/mco/js/aspect-working-copy.js
@@ -1,10 +1,6 @@
-(function($) { 
+(function($) {        
     $(document).ready(function(){ 
-    alert("HELLO WORLD");
-	  $(window).height();   // returns height of browser viewport
-	  document.write('cookie monster');
-      $(document).height(); // returns height of HTML document
-      $(window).width();   // returns width of browser viewport
-      $(document).width(); // returns width of HTML document
+	   $('p').hide();
+	   alert('hello');
+    });
 })(jQuery)
-})
